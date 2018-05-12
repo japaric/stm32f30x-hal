3b33407cfeb97fcce5e5944e4f61a5bf58059fa0
Jorge Aparicio <jorge@japaric.io>
2018-05-12 21:32:14 +0200

    implement embedded-hal v0.2.0 and make compilable on stable

--- src/lib.rs
+++ src/lib.rs
@@ -5,23 +5,26 @@
 //!
 //! [`embedded-hal`]: https://github.com/japaric/embedded-hal
 //!
+//! # Requirements
+//!
+//! This crate requires `arm-none-eabi-gcc` to be installed and available in `$PATH` to build.
+//!
 //! # Usage
 //!
 //! To build applications (binary crates) using this crate follow the [cortex-m-quickstart]
 //! instructions and add this crate as a dependency in step number 5 and make sure you enable the
 //! "rt" Cargo feature of this crate.
 //!
-//! [cortex-m-quickstart]: https://docs.rs/cortex-m-quickstart/~0.2.3
+//! [cortex-m-quickstart]: https://docs.rs/cortex-m-quickstart/~0.3
 //!
 //! # Examples
 //!
 //! Examples of *using* these abstractions can be found in the documentation of the [`f3`] crate.
 //!
-//! [`f3`]: https://docs.rs/f3/~0.5.1
+//! [`f3`]: https://docs.rs/f3/~0.6
 
 #![deny(missing_docs)]
 #![deny(warnings)]
-#![feature(never_type)]
 #![no_std]
 
 extern crate cast;
@@ -29,6 +32,7 @@ extern crate cortex_m;
 extern crate embedded_hal as hal;
 extern crate nb;
 pub extern crate stm32f30x;
+extern crate void;
 
 pub mod delay;
 pub mod flash;
